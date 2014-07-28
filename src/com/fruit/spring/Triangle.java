f0351ad0756dfeacd9339335ff6db00050279145
SHIAN HUANG <shian.huang@ftlinc.com>
2014-07-28 15:49:48 -0500

    added Point class and used point objects as properties in Triangle

--- src/com/fruit/spring/Triangle.java
+++ src/com/fruit/spring/Triangle.java
@@ -1,48 +1,33 @@
 package com.fruit.spring;
 
 public class Triangle {
-
-	private String type;
-	private int height;
-	private String hello;
 	
-	public Triangle( String type ) {
-		this.type = type;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	public Point getPointA() {
+		return pointA;
 	}
-	
-	public Triangle( String type, int height ) {
-		this.type = type;
-		this.height = height;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
-	
-	public Triangle(String type, int height, String hello) {
-		super();
-		this.type = type;
-		this.height = height;
-		this.hello = hello;
+	public Point getPointB() {
+		return pointB;
 	}
-
-	public Triangle( int height ) {
-		this.height = height;
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+	public Point getPointC() {
+		return pointC;
+	}
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 	
 	public void draw() {
-		System.out.println( getType() + " Triangle Drawn, the height is " + getHeight() + " hello content is " + getHello());
+		
+		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+		System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+		System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
 	}
-
-	public String getType() {
-		return type;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-	
-	public String getHello() {
-		return hello;
-	}
-
-//	public void setType(String type) {
-//		this.type = type;
-//	}
 }
