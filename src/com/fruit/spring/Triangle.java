5b18a296ad27295cc326946654f1ab2501ef6fed
SHIAN HUANG <shian.huang@ftlinc.com>
2014-07-28 15:19:43 -0500

    Using constructor config for private properties

--- src/com/fruit/spring/Triangle.java
+++ src/com/fruit/spring/Triangle.java
@@ -3,16 +3,46 @@ package com.fruit.spring;
 public class Triangle {
 
 	private String type;
+	private int height;
+	private String hello;
+	
+	public Triangle( String type ) {
+		this.type = type;
+	}
+	
+	public Triangle( String type, int height ) {
+		this.type = type;
+		this.height = height;
+	}
+	
+	public Triangle(String type, int height, String hello) {
+		super();
+		this.type = type;
+		this.height = height;
+		this.hello = hello;
+	}
+
+	public Triangle( int height ) {
+		this.height = height;
+	}
 	
 	public void draw() {
-		System.out.println( getType() + " Triangle Drawn");
+		System.out.println( getType() + " Triangle Drawn, the height is " + getHeight() + " hello content is " + getHello());
 	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
+	public int getHeight() {
+		return height;
 	}
+	
+	public String getHello() {
+		return hello;
+	}
+
+//	public void setType(String type) {
+//		this.type = type;
+//	}
 }
